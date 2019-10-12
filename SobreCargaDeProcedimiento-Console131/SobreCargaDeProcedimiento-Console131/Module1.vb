86f3adf882b07998b43a2374c6381b6852bcf8b2
NicolasGomez97 <nicolas_gomez21@outlook.com>
2019-10-12 14:05:12 -0300

    Try

---
+++ SobreCargaDeProcedimiento-Console131/SobreCargaDeProcedimiento-Console131/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        ' mostrar datos del empleado
+        ' en función del nombre
+        VerDatosEmpleado("Pedro")
+        ' mostrar datos del empleado
+        ' en función de la edad
+        VerDatosEmpleado(, 28)
+        Console.ReadLine()
+    End Sub
+    Sub VerDatosEmpleado(Optional ByVal Nombre As String = "X", Optional ByVal Edad As Integer = 999)
+        If Nombre <> "X" Then
+            Console.WriteLine("Nombre del empleado: {0}", Nombre)
+            Console.WriteLine("Domicilio: La Rioja 1450")
+            Console.WriteLine("Localidad: Córdoba")
+        End If
+        If Edad <> 999 Then
+            Console.WriteLine("Edad del empleado: {0}", Edad)
+            Console.WriteLine("DNI:21555666")
+            Console.WriteLine("Fecha de alta en la empresa: 10/4/1997")
+        End If
+        Console.WriteLine()
+    End Sub
+
+End Module
---
+++ SobreCargaDeProcedimiento-Console132/SobreCargaDeProcedimiento-Console132/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        ' mostrar datos del empleado según nombre
+        VerEmpleNombre("Pedro")
+        ' mostrar datos del empleado según edad
+        VerEmpleNum(28)
+        Console.ReadLine()
+    End Sub
+    Public Sub VerEmpleNombre(ByVal Nombre As String)
+        Console.WriteLine("Datos empleado por nombre")
+        Console.WriteLine("Nombre del empleado: {0}", Nombre)
+        Console.WriteLine("Domicilio: La Rioja 1450")
+        Console.WriteLine("Localidad: Córdoba")
+        Console.WriteLine()
+    End Sub
+    Public Sub VerEmpleNum(ByVal Edad As Integer)
+        Console.WriteLine("Datos empleado por edad")
+        Console.WriteLine("Edad del empleado: {0}", Edad)
+        Console.WriteLine("DNI:21555666")
+        Console.WriteLine("Fecha de alta en la empresa: 10/4/1997")
+        Console.WriteLine()
+    End Sub
+
+End Module
